24c847b76feb9cc7ba352f41dff1464ae3e22eba
Katrina Owen <katrina.owen@gmail.com>
2012-04-12 11:11:24 +0200

    Reorganize item

--- lib/item.rb
+++ lib/item.rb
@@ -7,10 +7,64 @@ class Item < ActiveRecord::Base
 
   serialize :histogram
 
-  def self.calculate_all
-    Item.all.each do |item|
-      item.refresh_from_acks!
+  class << self
+    def calculate_all
+      Item.all.each do |item|
+        item.refresh_from_acks!
+      end
     end
+
+    def pick(already_picked, resultset, number, random)
+      picked = []
+      until resultset.empty? || picked.size == number
+        pos = random ? rand(resultset.length) : 0
+        item = resultset.delete_at(pos)
+        picked << item unless already_picked.include?(item.id)
+      end
+      picked
+    end
+
+    def by_field(path, segment, sample_size, identity)
+      scope = Item.scoped.where(:path => path).order("#{segment[:field]} #{segment[:order]} NULLS LAST").limit(sample_size)
+
+      unless identity.nil? # if identity is given, exclude every items that this identity has voted for already
+        scope = scope
+        .joins("LEFT OUTER JOIN acks on acks.item_id = items.id and acks.identity=#{identity}")
+        .where("acks.id IS NULL")
+      end
+      #raise scope.to_sql
+      scope
+    end
+
+    def combine_resultsets(path, params)
+      picked = []
+      remaining = []
+      sampled = params.segments.map do |segment|
+
+        sample_size_percent = segment[:sample_size] || segment[:percent]
+        total = Item.count
+        sample_row_num = (total * 0.01 * sample_size_percent).ceil # how many rows to pick randomly from
+        sample_row_num = params.limit if sample_row_num < params.limit # always try to return <limit> items
+
+        results = Item.by_field(path, segment, sample_row_num, params.identity_id)
+        share_of_total = params.limit * segment[:percent] * 0.01
+
+        sampled = Item.pick(picked, results, share_of_total.ceil, params.shuffle)
+
+        picked |= sampled.map(&:id)
+        remaining.concat results
+        sampled
+      end
+      sampled.flatten!
+
+      diff = (params.limit - sampled.size).to_i
+      if diff > 0
+        sampled |= Item.pick(picked, remaining, diff, params.shuffle)
+      end
+      sampled.shuffle! if params.shuffle
+      sampled
+    end
+
   end
 
   def refresh_from_acks!
@@ -62,57 +116,6 @@ class Item < ActiveRecord::Base
       write_attribute(:controversiality, [positive_count, negative_count].min)
     end
     read_attribute(:controversiality)
-  end
-
-  def self.pick(already_picked, resultset, number, random)
-    picked = []
-    until resultset.empty? || picked.size == number
-      pos = random ? rand(resultset.length) : 0
-      item = resultset.delete_at(pos)
-      picked << item unless already_picked.include?(item.id)
-    end
-    picked
-  end
-
-  def self.by_field(path, segment, sample_size, identity)
-    scope = Item.scoped.where(:path => path).order("#{segment[:field]} #{segment[:order]} NULLS LAST").limit(sample_size)
-
-    unless identity.nil? # if identity is given, exclude every items that this identity has voted for already
-      scope = scope
-      .joins("LEFT OUTER JOIN acks on acks.item_id = items.id and acks.identity=#{identity}")
-      .where("acks.id IS NULL")
-    end
-    #raise scope.to_sql
-    scope
-  end
-
-  def self.combine_resultsets(path, params)
-    picked = []
-    remaining = []
-    sampled = params.segments.map do |segment|
-
-      sample_size_percent = segment[:sample_size] || segment[:percent]
-      total = Item.count
-      sample_row_num = (total * 0.01 * sample_size_percent).ceil # how many rows to pick randomly from
-      sample_row_num = params.limit if sample_row_num < params.limit # always try to return <limit> items
-
-      results = Item.by_field(path, segment, sample_row_num, params.identity_id)
-      share_of_total = params.limit * segment[:percent] * 0.01
-
-      sampled = Item.pick(picked, results, share_of_total.ceil, params.shuffle)
-
-      picked |= sampled.map(&:id)
-      remaining.concat results
-      sampled
-    end
-    sampled.flatten!
-
-    diff = (params.limit - sampled.size).to_i
-    if diff > 0
-      sampled |= Item.pick(picked, remaining, diff, params.shuffle)
-    end
-    sampled.shuffle! if params.shuffle
-    sampled
   end
 
   private
