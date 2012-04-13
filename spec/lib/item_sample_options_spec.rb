310b31a6caaedc0425f387bb31dfe748110ec12f
Katrina Owen <katrina.owen@gmail.com>
2012-04-13 12:02:24 +0200

    Tweak tests for readability

--- spec/lib/item_sample_options_spec.rb
+++ spec/lib/item_sample_options_spec.rb
@@ -18,33 +18,33 @@ describe ItemSampleOptions do
     its(:attributes) { should eq(limit: 10, records: 100, path: 'a.b.c', randomize: true, exclude_votes_by: 42, valid_filters: ['valid_filter']) }
   end
 
+  describe "comlicated truths" do
+  end
+
   describe "identity" do
 
-    context "exclusion" do
-      it "excludes votes" do
-        segment = ItemSampleOptions.new(default_options.merge(:exclude_votes_by => 42))
-        segment.exclude_votes_by.should eq(42)
-      end
+    it "excludes votes" do
+      segment = ItemSampleOptions.new(default_options.merge(:exclude_votes_by => 42))
+      segment.exclude_votes_by.should eq(42)
+    end
 
-      it "doesn't exclude if there's no id" do
-        segment = ItemSampleOptions.new(default_options.merge(:exclude_votes_by => nil))
-        segment.exclude_votes_by.should be_nil
-      end
+    it "can't exclude votes without an identity id" do
+      segment = ItemSampleOptions.new(default_options.merge(:exclude_votes_by => nil))
+      segment.exclude_votes_by.should be_nil
+    end
 
-      it "ignores deprecated fields" do
-        segment = ItemSampleOptions.new(default_options.merge(:identity_id => 1337, :include_own => false, :exclude_votes_by => nil))
-        segment.exclude_votes_by.should be_nil
-      end
-
+    it "ignores deprecated fields" do
+      segment = ItemSampleOptions.new(default_options.merge(:identity_id => 1337, :include_own => false, :exclude_votes_by => nil))
+      segment.exclude_votes_by.should be_nil
     end
 
     context "deprecated" do
-      it "excludes records voted on by identity" do
+      it "excludes votes" do
         segment = ItemSampleOptions.new(default_options.merge(:identity_id => 1337, :include_own => false))
         segment.exclude_votes_by.should eq(1337)
       end
 
-      it "doesn't exclude records" do
+      it "doesn't exclude votes" do
         segment = ItemSampleOptions.new(default_options.merge(:identity_id => 1337, :include_own => true))
         segment.exclude_votes_by.should be_nil
       end
