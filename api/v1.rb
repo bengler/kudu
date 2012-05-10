8a9737ea0c709b0ed383e1bd270fb1b8ec2636cc
Katrina Owen <katrina.owen@gmail.com>
2012-05-10 08:37:41 +0200

    Clean up API.

--- api/v1.rb
+++ api/v1.rb
@@ -48,32 +48,14 @@ class KuduV1 < Sinatra::Base
     pg :ack, :locals => {:ack => ack}
   end
 
+  # FIXME: Hack for dittforslag.
+  # It (incorrectly) returns the count of all acks in the system, not just for dittforslag.
   get '/acks/:uid/count' do |uid|
-    # TODO: Implement properly. For now just return the full count to get dittforslag.no out the door.
     {:uid => uid, :count => Ack.count,
       :note => "Not fully implemented! Returns the full ack count for all realms and paths always."}.to_json
   end
 
-  get '/acks/:uid/stats' do |uid|
-    # TODO: Implement properly. For now just return the full count to get dittforslag.no Admin STATS out the door.
-    total = Ack.count
-    positive = Ack.where("value > 0").count
-    negative = Ack.where("value < 0").count
-    unique_voters = Ack.select("distinct identity").count
-    avg_votes_per_voter = total.to_f / unique_voters.to_f
-
-    {
-      :uid => uid,
-      :positive_count => positive,
-      :negative_count => negative,
-      :total_count => total,
-      :unique_voters => unique_voters,
-      :avg_votes_per_voter => avg_votes_per_voter,
-      :note => "Not fully implemented! Returns the full ack count for all realms and paths always."
-    }.to_json
-  end
-
-  # Update a single Ack for current identity
+  # Update a single Ack for the current identity
   put '/acks/:uid' do |uid|
     require_identity
 
@@ -96,10 +78,9 @@ class KuduV1 < Sinatra::Base
     response.status = 204
   end
 
-  # Create a score (to preserve creation date for scores)
-  # This is idempotent
-  # todo: write tests
-  # todo: remove duplicate
+  # Create a score in order to preserve creation date.
+  # This is idempotent.
+  # FIXME: There are no tests for this code.
   put '/scores/:uid/touch' do |uid|
     require_identity
     score = Score.find_or_create_by_external_uid(uid)
@@ -111,7 +92,7 @@ class KuduV1 < Sinatra::Base
     "Ok"
   end
 
-  # Query for scores/summaries, this probably needs pagination
+  # TODO: Implement pagination
   get '/scores/:uids' do
     uids = params[:uids].split(",")
     scores = Score.find_all_by_external_uid(uids)
