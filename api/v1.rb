e6d8a8862b1db8c1a6c39127ca1d640f1392432e
Bjørge Næss <bjoerge@bengler.no>
2013-05-16 15:58:28 +0200

    Document the headers added to prevent caching in IE(9)

--- api/v1.rb
+++ api/v1.rb
@@ -14,8 +14,13 @@ class KuduV1 < Sinatra::Base
 
   before do
     response.headers['Cache-Control'] = 'public, max-age=300'
+
+    # If this service, for some reason lives behind a proxy that rewrites the Cache-Control headers into
+    # "must-revalidate" (which IE9, and possibly other IEs, does not respect), these two headers should properly prevent
+    # caching in IE (see http://support.microsoft.com/kb/234067)
     headers 'Pragma' => 'no-cache'
     headers 'Expires' => '-1'
+
     cache_control :private, :no_cache, :no_store, :must_revalidate
   end
 
