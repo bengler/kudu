8f02340bc6228f5b76dfd7da7632c0d410858d0a
Thomas Drevon <thomas@bengler.no>
2018-02-02 14:27:31 +0100

    Put back that aggressive logging, now with cookies too

--- api/v1/acks.rb
+++ api/v1/acks.rb
@@ -65,6 +65,7 @@ class KuduV1 < Sinatra::Base
   # @optional [String] identity Checkpoint identity id.
   # @status 200 JSON
   get '/acks/:external_uid/:kind' do |uid, kind|
+    LOGGER.info("---------\nParams: #{params.inspect}\ncurrent_identity_data: #{current_identity_data.inspect}\ncurrent_session: #{current_session}\nPebbles: #{pebbles.inspect}\nCookies: #{request.cookies.inspect}\n---------\n")
 
     if params[:identity]
       require_god
